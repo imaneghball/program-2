cef47f4e47b13a268759cc05846d8abe0b1e112a
imaneghball <92173717+imaneghball@users.noreply.github.com>
2022-07-07 16:25:55 +0200

    Delete StringMovie.py
    lö

--- StringMovie.py
+++
@@ -1,9 +0,0 @@
-
-start="i am iman"
-start=start.split()
-start.reverse()
-print(start)
-def retur(satz):
-    #return "-_".join(reversed(satz.split()))
-    return "*".join(satz.split()[::-1])
-print(retur("i am iman"))
